3b06db8e5d1ff90a2cb07c7691bcc7495d7426af
Fernando Aponte <fernandoaponte90@gmail.com>
2022-05-12 20:25:15 -0400

    feat: add commitizen/eslint/prettier/husky config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,35 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
-    sourceType: 'module',
+  env: {
+    browser: true,
+    es2021: true
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    'standard',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: [
+    '@typescript-eslint',
+    '@typescript-eslint/eslint-plugin'
+
+  ],
+  ignorePatterns: ['.eslintrc.js'],
   root: true,
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
   },
-};
+}
